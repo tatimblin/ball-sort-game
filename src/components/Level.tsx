50854ebcd09b63355c10f22211bb5f698b9ae1e3
tatimblin@gmail.com <tatimblin@gmail.com>
2022-04-09 13:20:22 -0400

    remove unused variable

--- src/components/Level.tsx
+++ src/components/Level.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from './Container';
 import { Game, IGame } from '../utils/Game';
 
